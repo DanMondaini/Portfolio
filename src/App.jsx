e10d9de98440128843672a9db75adb005ca47bf8
Daniel Mondaini <danielmondaini@outlook.com>
2022-08-25 18:37:28 -0300

    fix: sass app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import  './styles/components/app.sass'
 function App() {
 
   return (
-    <div id="portfolio">
+    <div className='App'>
       <h1>Daniel Mondaini</h1>
       <Sidebar />
       <MainContent />
